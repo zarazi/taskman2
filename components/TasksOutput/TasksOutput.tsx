3139294b108e60b64ad3b57202d4a5eaee8ff2aa
zarazi <zarazi@hotmail.com>
2024-11-20 00:54:42 +0700

    Adding dummy task data

--- components/TasksOutput/TasksOutput.tsx
+++ components/TasksOutput/TasksOutput.tsx
@@ -1,6 +1,34 @@
 import { View } from "react-native";
 import TasksList from "./TasksList";
 
+interface ITask {
+  id: string;
+  title: string;
+  description: string;
+  status: string;
+}
+
+const DUMMY_TASKS: ITask[] = [
+  {
+    id: "1",
+    title: "Research todos app",
+    description: "something worth spending ...",
+    status: "todo",
+  },
+  {
+    id: "2",
+    title: "Design app architecture",
+    description: "something worth spending more ...",
+    status: "todo",
+  },
+  {
+    id: "3",
+    title: "Prep coding tools",
+    description: "something worth spending much more ...",
+    status: "todo",
+  },
+];
+
 function TasksOutput() {
   return (
     <View>
