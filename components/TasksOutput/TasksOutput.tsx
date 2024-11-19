12e01545e3da1cbb94c0d292abe5a5377d991485
zarazi <zarazi@hotmail.com>
2024-11-20 00:49:11 +0700

    Added tasks output component

---
+++ components/TasksOutput/TasksOutput.tsx
@@ -0,0 +1,12 @@
+import { View } from "react-native";
+import TasksList from "./TasksList";
+
+function TasksOutput() {
+  return (
+    <View>
+      <TasksList />
+    </View>
+  );
+}
+
+export default TasksOutput;
