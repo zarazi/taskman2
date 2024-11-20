8a6f63ff559d4eb6b7b900e19ff32b348e50d758
zarazi <zarazi@hotmail.com>
2024-11-21 02:02:17 +0700

    Closing modals

--- screens/ManageTask.tsx
+++ screens/ManageTask.tsx
@@ -5,6 +5,7 @@ import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { StackParamList } from "../@types/navigation";
 import { GlobalStyles } from "../constants/styles";
 import IconButton from "../components/UI/IconButton";
+import Button from "../components/UI/Button";
 
 type ManageTaskProps = NativeStackScreenProps<StackParamList, "ManageTask">;
 
@@ -18,10 +19,28 @@ function ManageTask({ route, navigation }: ManageTaskProps) {
     });
   }, [navigation, isEditing]);
 
-  function deleteTaskHandler() {}
+  function deleteTaskHandler() {
+    navigation.goBack();
+  }
+
+  function cancelHandler() {
+    navigation.goBack();
+  }
+
+  function confirmHandler() {
+    navigation.goBack();
+  }
 
   return (
     <View style={styles.container}>
+      <View style={styles.buttons}>
+        <Button style={styles.button} mode="flat" onPress={cancelHandler}>
+          Cancel
+        </Button>
+        <Button style={styles.button} onPress={confirmHandler}>
+          {isEditing ? "Update" : "Add"}
+        </Button>
+      </View>
       {isEditing && (
         <View style={styles.deleteContainer}>
           <IconButton
@@ -43,6 +62,15 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 24,
     backgroundColor: GlobalStyles.colors.primary800,
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
   },
   deleteContainer: {
     marginTop: 16,
