6743ae78c3df76e467855b5e85cc6cfee7d0ee43
zarazi <zarazi@hotmail.com>
2024-11-21 01:32:52 +0700

    Adding a delete button

--- screens/ManageTask.tsx
+++ screens/ManageTask.tsx
@@ -1,8 +1,10 @@
 import { useLayoutEffect } from "react";
-import { Text } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 
 import { StackParamList } from "../@types/navigation";
+import { GlobalStyles } from "../constants/styles";
+import IconButton from "../components/UI/IconButton";
 
 type ManageTaskProps = NativeStackScreenProps<StackParamList, "ManageTask">;
 
@@ -16,7 +18,37 @@ function ManageTask({ route, navigation }: ManageTaskProps) {
     });
   }, [navigation, isEditing]);
 
-  return <Text>ManageTask Screen</Text>;
+  function deleteTaskHandler() {}
+
+  return (
+    <View style={styles.container}>
+      {isEditing && (
+        <View style={styles.deleteContainer}>
+          <IconButton
+            name="trash"
+            color={GlobalStyles.colors.error500}
+            size={36}
+            onPress={deleteTaskHandler}
+          />
+        </View>
+      )}
+    </View>
+  );
 }
 
 export default ManageTask;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary800,
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: "center",
+  },
+});
