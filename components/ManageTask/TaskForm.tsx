b2e02a222c0ce96742639be3ad1a3b839a9833e0
zarazi <zarazi@hotmail.com>
2024-11-21 10:32:05 +0700

    Handle user inputs

--- components/ManageTask/TaskForm.tsx
+++ components/ManageTask/TaskForm.tsx
@@ -1,7 +1,28 @@
 import { View, Text, StyleSheet } from "react-native";
 import Input from "./Input";
+import { useEffect, useState } from "react";
+import { ITaskData } from "../../@types/task";
 
-function TaskForm() {
+function TaskForm(this: any) {
+  const [inputValues, setInputValues] = useState<ITaskData>({
+    title: "",
+    description: "",
+    status: "",
+  });
+
+  function inputChangedHandler(inputIdentifier: string, enteredValue: string) {
+    setInputValues((currentInputValues) => {
+      return {
+        ...currentInputValues,
+        [inputIdentifier]: enteredValue,
+      };
+    });
+  }
+
+  // useEffect(() => {
+  //   console.log(inputValues);
+  // }, [inputValues]);
+
   return (
     <View style={styles.form}>
       <Text style={styles.title}>Your Task</Text>
@@ -9,7 +30,8 @@ function TaskForm() {
         label="Title"
         textInputConfig={{
           maxLength: 100,
-          onChangeText: () => {},
+          onChangeText: inputChangedHandler.bind(this, "title"),
+          value: inputValues.title,
         }}
       />
       <Input
@@ -17,7 +39,8 @@ function TaskForm() {
         textInputConfig={{
           multiline: true,
           maxLength: 255,
-          onChangeText: () => {},
+          onChangeText: inputChangedHandler.bind(this, "description"),
+          value: inputValues.description,
           // autoCapitalize: 'none',
           // autoCorrect: true,
         }}
@@ -26,7 +49,8 @@ function TaskForm() {
         label="Status"
         textInputConfig={{
           maxLength: 12,
-          onChangeText: () => {},
+          onChangeText: inputChangedHandler.bind(this, "status"),
+          value: inputValues.status,
         }}
       />
     </View>
@@ -37,14 +61,14 @@ export default TaskForm;
 
 const styles = StyleSheet.create({
   form: {
-    marginTop: 64,
+    // marginTop: 64,
     marginBottom: 16,
   },
   title: {
     fontSize: 24,
     fontWeight: "bold",
     color: "white",
-    marginVertical: 24,
+    // marginVertical: 24,
     textAlign: "center",
   },
 });
